2bbf108716363e1a6cc074ce833e3f2641521a7a
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-15 04:55:07 +0600

    added login form

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+import ServicesCard from "./ServicesCard";
+
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
   return (
     <div className="mt-4">
       <div className="text-center">
@@ -9,6 +20,11 @@ const Services = () => {
           humour, or randomized <br /> words which look even slightly
           believable.
         </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {services.map((service) => (
+          <ServicesCard key={service._id} service={service}></ServicesCard>
+        ))}
       </div>
     </div>
   );
